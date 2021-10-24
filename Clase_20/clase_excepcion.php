0996051dcd55ced8830ddadd121d75e7088da248
nahuelmontesino <montesinonahuel@gmail.com>
2021-10-24 15:53:34 -0300

    se agregan comentarios

--- Clase_20/clase_excepcion.php
+++ Clase_20/clase_excepcion.php
@@ -1,7 +1,7 @@
 <?php
     class customException extends Exception {
         public function errorEmailMessage() {
-            //error message
+            // Mensaje de error personalizado, utilizando el email que recibimos como parametro.
             $errorMsg = 'Error '.': La direccion: <b>'.$this->getMessage().'</b> no es una direccion de email valida';
             return $errorMsg;
         }
@@ -10,15 +10,17 @@
     $email = "someone@example...com";
 
     try {
-        //Verifica que el string del email que se paso cumple con las caracteristicas basicas de un email
+        // Verifica que el string del email que se paso cumple con las caracteristicas basicas de un email
         if(filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) {
             //Lanza una excepcion en caso de que no cumpla con la sintaxis basica
             throw new customException($email);
         }
     }
 
+    // Capturamos las excepciones del tipo customException
     catch (customException $e) {
-        //Muestra un mensaje personalizado
+
+        //Muestra el mensaje personalizado de nuestra clase personalizada
         echo $e->errorEmailMessage();
     }
 ?>
